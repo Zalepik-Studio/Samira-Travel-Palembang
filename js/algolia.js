a6655a0aba7f52f0334a8d585a2d5ea5b161551b
Zen Zalepik <51511976+zenzalepik@users.noreply.github.com>
2024-01-29 00:01:35 +0700

    google analitic

--- js/algolia.js
+++ js/algolia.js
@@ -1,7 +1,7 @@
 (() => {
   // ns-params:@params
   var baseURL = "/";
-  var params = { defaultcover: "", email: "palembangsamiratravel@gmail.com", images: ["site-feature-image.jpg"], showcopyright: true, showmore: true, showrss: true, socialmedia: [{ name: "Instagram", url: "https://www.instagram.com/samiratravel.palembang/" }, { name: "Whatsapp", url: "https://api.whatsapp.com/send?phone=6281377886689&text=Assalamu%27alaikum.%20Perkenalkan%20saya%20..." }, { name: "Facebook", url: "https://www.facebook.com/samira.travelofficial" }, { name: "Youtube", url: "https://www.youtube.com/channel/UChHnjnMy5dleNbhYOnnV6Yg" }], telephone: "081377886689" };
+  var params = { defaultcover: "https://hosting-images-samiratravel-palembang.netlify.app/logo-samira-travel-palembang.jpg", email: "palembangsamiratravel@gmail.com", images: ["https://hosting-images-samiratravel-palembang.netlify.app/logo-samira-travel-palembang.jpg"], showcopyright: true, showmore: true, showrss: true, socialmedia: [{ name: "Instagram", url: "https://www.instagram.com/samiratravel.palembang/" }, { name: "Whatsapp", url: "https://api.whatsapp.com/send?phone=6281377886689&text=Assalamu%27alaikum.%20Perkenalkan%20saya%20..." }, { name: "Facebook", url: "https://www.facebook.com/samira.travelofficial" }, { name: "Youtube", url: "https://www.youtube.com/channel/UChHnjnMy5dleNbhYOnnV6Yg" }], telephone: "081377886689" };
 
   // <stdin>
   var { appid, appkey, searchindex: indexName, enabled } = params.algolia;
